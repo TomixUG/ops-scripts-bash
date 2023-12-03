d8dd9da69218cda9f71015dbd879eee6a3692a33
TomixUG <tomas.sousek1@gmail.com>
2023-12-03 16:35:18 +0100

    improve first script

--- 1/systeminfo.sh
+++ 1/systeminfo.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+# funkce na hezky vypis
+display_success() {
+	echo -e "\033[0;32mSuccess:\033[0m $1"
+	echo "$1" >>log.txt
+}
+
+display_info() {
+	echo -e "\033[0;33mInfo:\033[0m $1"
+	echo "$1" >>log.txt
+}
+
 newLine() {
 	echo "" >>systeminfo.txt
 }
 
+display_info "Processing date..."
 echo "Aktualni datum a cas: " >systeminfo.txt
 date >>systeminfo.txt
 newLine
 
+display_info "Processing current user info..."
 echo "Aktualne prihlaseny uzivatel: " >>systeminfo.txt
 whoami >>systeminfo.txt
 
 newLine
 
+display_info "Processing system info..."
 echo "Systemove informace:" >>systeminfo.txt
 echo "- kernel info: " >>systeminfo.txt
 uname -a >>systeminfo.txt
@@ -27,12 +41,16 @@ cat /proc/cpuinfo | grep 'model name' | uniq | awk -F': ' '/model name/ {print $
 
 newLine
 
+echo "" >>systeminfo.txt
+echo "" >>systeminfo.txt
+display_info "Processing paths..."
 echo "Seznam vyhledavacich cest:" >>systeminfo.txt
 echo $PATH >>systeminfo.txt
 
 newLine
 
+display_info "Processing env variables..."
 echo "Seznam a hodnoty systemovych promennych:" >>systeminfo.txt
 printenv >>systeminfo.txt
 
-top >>systeminfo.txt
+display_success "Output is in systeminfo.txt"
